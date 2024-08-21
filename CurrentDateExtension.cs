b5857fb85426a695ee31e3900e634b63a1cf8899
User <agbisit.andreijoaqhimali@auf.edu.ph>
2024-08-21 12:19:28 +0800

    Add project files.

---
+++ CurrentDateExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module01Activity02
+{
+    public class CurrentDateExtension : IMarkupExtension
+    {
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return DateTime.Now.ToString("D");
+
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace Module01Activity02
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Firstname { get; set; }
+        public Color ColorRed { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Firstname = "Hello, MAUI!";
+            ColorRed = Colors.Red;
+
+            this.BindingContext = this;
+
+        }
+
+        private void OnChangedLabelName(object sender, EventArgs e)
+        {
+            this.Resources["StaticUsername"] = "Peter Butter";
+        }
+
+        private void OnChangedLabelNameDynamic(object sender, EventArgs e)
+        {
+            this.Resources["DynamicUsername"] = "Mr. Juan dela Cruz";
+        }
+
+        private void OnChangeBackgroundColor(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.LightGray;
+        }
+
+        private void OnChangeBackgroundColorGreen(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Green;
+        }
+
+        private void OnChangeBackgroundColorLBlue(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.LightBlue;
+        }
+
+        private void OnChangeBoxViewColor(object sender, EventArgs e)
+        {
+            this.Resources["DynamicBoxColor"] = Colors.Red;
+        }
+
+    }
+
+}
